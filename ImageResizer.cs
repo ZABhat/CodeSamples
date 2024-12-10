cb6ef1048aeee1b625141afd4dc0830bc5d12929
ZABhat <93826142+ZABhat@users.noreply.github.com>
2024-12-10 15:54:08 +0530

    uploaded ImageResizer

---
+++ ImageResizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Graphics.Platform;
+using SkiaSharp;
+
+namespace QuickstartConsole
+{
+    
+    public static class ImageResizer
+    {
+        public static byte[] ResizeImage(byte[] imageData, int width, int height)
+        {
+            using var inputStream = new MemoryStream(imageData);
+            using var original = SKBitmap.Decode(inputStream);
+
+            if (original == null)
+                return null;
+
+            var resized = original.Resize(new SKImageInfo(width, height), SKFilterQuality.High);
+            if (resized == null)
+                return null;
+
+            using var image = SKImage.FromBitmap(resized);
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+
+            return data.ToArray();
+        }
+    }
+
+}
