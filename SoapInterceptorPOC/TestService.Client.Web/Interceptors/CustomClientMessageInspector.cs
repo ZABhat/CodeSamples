d2689eafeebe14ee6bf90e929ef00a83a839b49a
Zahid Amin Bhat <zahid.bhat.ext@thesiscloud.com>
2025-04-14 20:39:17 +0530

    soapMessageInterceptor

---
+++ SoapInterceptorPOC/TestService.Client.Web/Interceptors/CustomClientMessageInspector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ServiceModel.Dispatcher;
+using System.IO;
+using System.ServiceModel;
+using System.Xml;
+
+namespace TestService.Client.Web.Interceptors
+{
+
+
+    
+    public class CustomClientMessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            // Create a buffered copy of the reply message
+            var buffer = reply.CreateBufferedCopy(int.MaxValue);
+            var copy = buffer.CreateMessage();
+            reply = buffer.CreateMessage(); // Restore the original message
+
+            // Log the incoming response
+            LogMessage(copy, "C:\\temp\\IncomingResponse.log");
+        }
+
+        public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, IClientChannel channel)
+        {
+            // Create a buffered copy of the request message
+            var buffer = request.CreateBufferedCopy(int.MaxValue);
+            var copy = buffer.CreateMessage();
+            request = buffer.CreateMessage(); // Restore the original message
+
+            // Log the outgoing request
+            LogMessage(copy, "C:\\temp\\OutgoingRequest.log");
+            return null;
+        }
+
+
+        private void LogMessage(System.ServiceModel.Channels.Message message, string fileName)
+        {
+            using (var writer = new StreamWriter(fileName, true))
+            {
+                writer.WriteLine($"{DateTime.Now} : ");
+                using (var xmlWriter = XmlWriter.Create(writer))
+                {
+                    message.WriteMessage(xmlWriter);
+                }
+                writer.WriteLine();
+            }
+
+        }
+
+    }
+
+
+}
---
+++ SoapInterceptorPOC/TestService.Client.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestService.Client.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GlobalLogic")]
+[assembly: AssemblyProduct("TestService.Client.Web")]
+[assembly: AssemblyCopyright("Copyright © GlobalLogic 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d6ae327c-7b7c-4bf3-bfe7-b36a3a156a5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
